834ac7a1e0bfb4eb8f173739d415143e46ce384b
AlessioSalvaggio97 <137321118+AlessioSalvaggio97@users.noreply.github.com>
2023-06-22 08:47:29 +0200

    Add files via upload

---
+++ Utente.java
@@ -0,0 +1,41 @@
+package Autenticazione;
+
+public class Utente {
+	private String nome;
+	private String cognome;
+	private String indirizzo;
+	private int telefono;
+	//Vanno inseriti gli ID?
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getCognome() {
+		return cognome;
+	}
+	
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	
+	public String getIndirizzo() {
+		return indirizzo;
+	}
+	
+	public void setIndirizzo(String indirizzo) {
+		this.indirizzo = indirizzo;
+	}
+	
+	public int getTelefono() {
+		return telefono;
+	}
+	
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+}
