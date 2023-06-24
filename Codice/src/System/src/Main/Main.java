757f4d38c78b782466649ee1d7234b86ad767eaf
TechShooter <cloud019998@gmail.com>
2023-06-24 23:30:18 +0200

    Create Main.java

---
+++ Codice/src/System/src/Main/Main.java
@@ -0,0 +1,13 @@
+package Main;
+
+import Autenticazione.ModuloLogin;
+import Autenticazione.Utente;
+import GestioneFamiglie.RegistraFamiglia;;
+
+public class Main {
+    public static void main(String[] args) {
+        ModuloLogin log = new ModuloLogin();
+        Utente utente = new Utente();
+        RegistraFamiglia reg = new RegistraFamiglia();
+    }
+}
