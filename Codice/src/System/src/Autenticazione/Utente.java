674ab2025910eb53a77682a06945c173f94fd2b7
AlessioSalvaggio97 <137321118+AlessioSalvaggio97@users.noreply.github.com>
2023-06-26 12:08:25 +0200

    Add files via upload

---
+++ Codice/src/System/src/Autenticazione/Utente.java
@@ -0,0 +1,58 @@
+package Autenticazione;
+
+public class Utente {
+	private int ID_U;
+	private String nome;
+	private String cognome;
+	private String indirizzo;
+	private String ruolo;
+	private int telefono;
+	
+	public int getID_U(){
+		return ID_U;
+	}
+	
+	public void setID_U(int ID_U) {
+		this.ID_U = ID_U;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getCognome() {
+		return cognome;
+	}
+	
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	
+	public String getIndirizzo() {
+		return indirizzo;
+	}
+	
+	public void setIndirizzo(String indirizzo) {
+		this.indirizzo = indirizzo;
+	}
+	
+	public String getRuolo() {
+		return ruolo;
+	}
+	
+	public void setRuolo(String ruolo) {
+		this.ruolo = ruolo;
+	}
+	
+	public int getTelefono() {
+		return telefono;
+	}
+	
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+}
