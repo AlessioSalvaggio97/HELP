62fb5cee7e14d35c89a02ebc17156312933c9e9e
TechShooter <cloud019998@gmail.com>
2023-06-27 17:12:13 +0200

    Update ConnectionClass.java

--- Codice/src/System/src/Connectivity/ConnectionClass.java
+++ Codice/src/System/src/Connectivity/ConnectionClass.java
@@ -5,13 +5,11 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionClass {
-    // Connect to your database.
-    // Replace server name, username, and password with your credentials
-
+    
     public Connection getConnection() throws Exception{
         Connection conn = null;
         Class.forName("com.mysql.cj.jdbc.Driver");
-        conn = DriverManager.getConnection("jdbc:mysql://localhost/Database","root", "donazioni");
+        conn = DriverManager.getConnection("jdbc:mysql://localhost/dbms","root", "donazioni");
         System.out.println("Database connesso !");
         return conn;
     }
