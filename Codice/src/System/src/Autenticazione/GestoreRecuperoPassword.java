d8df003bd6cacac42e340fdcc15fe08fe0457015
AlessioSalvaggio97 <137321118+AlessioSalvaggio97@users.noreply.github.com>
2023-07-02 18:30:29 +0200

    Update

--- Codice/src/System/src/Autenticazione/GestoreRecuperoPassword.java
+++ Codice/src/System/src/Autenticazione/GestoreRecuperoPassword.java
@@ -7,5 +7,54 @@ public class GestoreRecuperoPassword {
     private ModuloOTP mOTP;
     private ModuloNuovaPassword mnp;
     private DBMSInterface db;
+    private ModuloLogin login;
+    private boolean isRegistered;
+    private Utente u;
+    private PannelloErroreMail pannErrMail;
+    private PannelloErroreOTP pannErrOTP;
+    private PannelloErroreRequisiti pannErrReq;
+
+
+    public GestoreRecuperoPassword(ModuloLogin login, DBMSInterface db){
+    this.login = login;
+    this.db = db;
+
+    gestisciRecupero();
+    }
+
+    public void gestisciRecupero(){
+        mr = new ModuloRecuperoPassword(db);
+        
+        isRegistered = db.verificaCredenziali(email);
+
+        while(isRegistered==false){
+            pannErrMail = new PannelloErroreMail();
+            mr = new ModuloRecuperoPassword(db);
+            isRegistered = db.verificaCredenziali(email);
+        }
+
+
+        mOTP = new ModuloOTP();
+        isRegistered = verificaOTP(otp); 
+
+        while(isCorrect==false){
+            pannErrOTP = new PannelloErroreOTP();
+            mOTP = new ModuloOTP();
+            isCorrect = db.verificaOTP(otp);
+        }
+
+        mnp = new ModuloNuovaPassword();
+        checkRequirement = verificaRequisiti(password);
+
+        while(checkRequirement == false){
+            pannErrReq = new PannelloErroreRequisiti();
+            mnp = new ModuloNuovaPassword();
+            checkRequirement = db.verificaRequisiti;
+        }
+
+        moduloLogin.setVisible(true);
+
+    }
+
 
 }
